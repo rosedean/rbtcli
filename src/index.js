cb675adf3ef37a4346119978af6dd01881113888
张国华 <rosedean@126.com>
2023-05-27 13:02:48 +0800

    feat: 增加update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ var yargs = require("yargs");
 var info = require("./info.js");
 
 var args = yargs
-    .command({
+    .command(
+        {
         command: "create <name>",
         desc: "Create a rbt_base_frame.",
         builder: {},
@@ -32,6 +33,36 @@ var args = yargs
                                     //7.修改项目的信息
                                     info.editProjectInfo(project);
                                 })
+                            })
+                        })
+                    })
+                })
+            })
+        }
+    })
+    .command(
+        {
+        command: "update <name>",
+        desc: "update a rbt_base_frame.",
+        builder: {},
+        handler: function(argv) {
+            var projectName = argv.name;
+            //1.获取模板列表
+            info.getTemplates(function(templates){
+                //2.设置选项
+                info.showTemplateList(templates, (templateName) => {
+                    var t = templates.find((template) => {
+                        return template.name == templateName;
+                    });
+                    //3.获取标签列表
+                    info.getTags(t.tagsUrl, (tags) => {
+                        //4.设置选项
+                        info.showTagList(tags, (tagName) => {
+                            var tag = tags.find((tag) => {
+                                return tag.name == tagName;
+                            });
+                                //5.下载zip_ball，并复制到目标位置
+                                info.downloadZipball(tag.zipUrl, projectName, () => {
                             })
                         })
                     })
